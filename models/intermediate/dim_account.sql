c18e449d318bc79df545020a89994e0cc9292e46
kds-student <student@kahandatasolutions.com>
2023-06-06 16:58:24 +0000

    Add dim_account

--- models/intermediate/dim_account.sql
+++ models/intermediate/dim_account.sql
@@ -8,15 +8,16 @@ account as (
 
 dim_account as 
 (
-    account_id,
-    is_deleted,
-    company_size,
-    created_date,
-    company_industry,
-    number_of_employees,
-    is_no_of_employees_valid,
-    company_type,
-    is_new_logo
+    select
+        account_id,
+        is_deleted,
+        company_size,
+        created_date,
+        company_industry,
+        number_of_employees,
+        is_no_of_employees_valid,
+        company_type
+    from account
 )
 
 select * from dim_account
